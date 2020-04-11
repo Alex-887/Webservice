9ddaeb7c68bfafcedfbb22ded1e91e43d44b0c7c
Léonard <leo1095@gmail.com>
2020-04-11 14:44:44 +0200

    First commit

---
+++ Webservice_Berthouzoz_Favre/Webservice_Berthouzoz_Favre/IWebservice_Berthouzoz_Favre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Webservice_Berthouzoz_Favre
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWebservice_Berthouzoz_Favre" in both code and config file together.
+    [ServiceContract]
+    public interface IWebservice_Berthouzoz_Favre
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
---
+++ Webservice_Berthouzoz_Favre/Webservice_Berthouzoz_Favre/Webservice_Berthouzoz_Favre.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Webservice_Berthouzoz_Favre
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Webservice_Berthouzoz_Favre" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Webservice_Berthouzoz_Favre.svc or Webservice_Berthouzoz_Favre.svc.cs at the Solution Explorer and start debugging.
+    public class Webservice_Berthouzoz_Favre : IWebservice_Berthouzoz_Favre
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
